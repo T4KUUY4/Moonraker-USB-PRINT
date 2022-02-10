114abbddd2f8817f3bc1982210ce0011e6f20edf
T4KUUY4 <54855101+T4KUUY4@users.noreply.github.com>
2022-02-10 21:43:42 +0100

    Add files via upload

--- usb-print.sh
+++ usb-print.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
 echo "Installing USB Print by Takuya#0523 | Please wait"
+
 sudo apt install usbmount
 mkdir "/home/"$USER"/gcode_files/USB"
+mkdir "/home/pi/gcode_files/USB"
+echo "Configuring USBMOUNT"
+wget https://raw.githubusercontent.com/T4KUUY4/Moonraker-USB-PRINT/main/usbmount.conf
+#wget https://raw.githubusercontent.com/rbrito/usbmount/master/usbmount.conf
+sudo mv usbmount.conf /etc/usbmount/
+wget https://raw.githubusercontent.com/T4KUUY4/Moonraker-USB-PRINT/main/usbmount && mv /usr/share/usbmount/
+sed 's/PrivateMounts=Yes/PrivateMounts=no/' /lib/systemd/system/systemd-udevd.service
+clear
+echo "DONE!"
